81b48a15a76aa895316440a51a9e568ed204ee02
Jan-Hendrik Ruberg <j-h.ruberg@elo.com>
2019-10-25 08:59:05 +0200

    UnitTests/common/[lib] sol.unittest.ix.services.SolCommonUserProfileUtils.js

--- UnitTests/common/[lib] sol.unittest.ix.services.SolCommonUserProfileUtils.js
+++ UnitTests/common/[lib] sol.unittest.ix.services.SolCommonUserProfileUtils.js
@@ -1,30 +1,13 @@
 
 describe("[lib] sol.unittest.ix.services.SolCommonUserProfileUtils", function () {
-  var UserProfileUtilsSord, userName, userInfo, originalTimeout, optionValue, bitMask, flag;
+  var originalTimeout, optionValue, bitMask, flag;
 
   beforeAll(function (done) {
     originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
     expect(function () {
       test.Utils.createTempSord("SolCommonUserProfileUtils").then(function success(obSolCommonUserProfileUtilsId) {
-        test.Utils.getSord("ARCPATH:/Administration/Business Solutions/common [unit tests]/Resources/UserProfileUtils").then(function success1(UserProfileUtilsSord1) {
-          UserProfileUtilsSord = UserProfileUtilsSord1;
-          userName = test.Utils.getCurrentUserName();
-          test.Utils.getUserInfo(userName).then(function success3(userInfo1) {
-            userInfo = userInfo1;
-            done();
-          }, function error(err) {
-            fail(err);
-            console.error(err);
-            done();
-          }
-          );
-        }, function error(err) {
-          fail(err);
-          console.error(err);
-          done();
-        }
-        );
+        done();
       }, function error(err) {
         fail(err);
         console.error(err);
@@ -35,16 +18,17 @@ describe("[lib] sol.unittest.ix.services.SolCommonUserProfileUtils", function ()
   });
   describe("Test Lib Functions", function () {
     describe("sol.common.UserProfileUtils", function () {
-      xit("isOptionBitSet", function (done) {
+      it("isOptionBitSet", function (done) {
         expect(function () {
-          optionValue = PVALUE;
-          bitMask = PVALUE;
+          optionValue = 7;
+          bitMask = 4;
           test.Utils.execute("RF_sol_unittest_service_ExecuteLib", {
             className: "sol.common.UserProfileUtils",
             classConfig: {},
             method: "isOptionBitSet",
             params: [optionValue, bitMask]
           }).then(function success(jsonResult) {
+            expect(jsonResult).toEqual(true);
             done();
           }, function error(err) {
             fail(err);
@@ -54,17 +38,39 @@ describe("[lib] sol.unittest.ix.services.SolCommonUserProfileUtils", function ()
           );
         }).not.toThrow();
       });
-      xit("setOptionBit", function (done) {
+      it("setOptionBit", function (done) {
         expect(function () {
-          optionValue = PVALUE;
-          flag = PVALUE;
-          bitMask = PVALUE;
+          optionValue = 3;
+          flag = true;
+          bitMask = 4;
           test.Utils.execute("RF_sol_unittest_service_ExecuteLib", {
             className: "sol.common.UserProfileUtils",
             classConfig: {},
             method: "setOptionBit",
             params: [optionValue, flag, bitMask]
           }).then(function success(jsonResult) {
+            expect(jsonResult).toEqual(7);
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("setOptionBit", function (done) {
+        expect(function () {
+          optionValue = 53;
+          flag = false;
+          bitMask = 4;
+          test.Utils.execute("RF_sol_unittest_service_ExecuteLib", {
+            className: "sol.common.UserProfileUtils",
+            classConfig: {},
+            method: "setOptionBit",
+            params: [optionValue, flag, bitMask]
+          }).then(function success(jsonResult) {
+            expect(jsonResult).toEqual(49);
             done();
           }, function error(err) {
             fail(err);
