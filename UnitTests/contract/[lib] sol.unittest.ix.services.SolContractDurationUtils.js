7889f38eb6665e5ec36294f5afec632b4fc33177
Jan-Hendrik Ruberg <j-h.ruberg@elo.com>
2019-11-15 15:39:02 +0100

    UnitTests/contract/[lib] sol.unittest.ix.services.SolContractDurationUtils.js

---
+++ UnitTests/contract/[lib] sol.unittest.ix.services.SolContractDurationUtils.js
@@ -0,0 +1,460 @@
+
+describe("[lib] sol.unittest.ix.services.SolContractDurationUtils", function () {
+  var DurationUtilsSord, userName, userInfo, originalTimeout, tplSord, updates,
+      mom, endOf, additionalShiftCounter, startDateIso, endDateIso, unit, amount,
+      exchangeRate, singleAmount, str, separator, unitStrings, isoDate, update;
+
+  beforeAll(function (done) {
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
+    expect(function () {
+      test.Utils.createTempSord("SolContractDurationUtils").then(function success(obSolContractDurationUtilsId) {
+        test.Utils.getSord("ARCPATH:/Administration/Business Solutions/contract [unit tests]/Resources/DurationUtils").then(function success1(DurationUtilsSord1) {
+          DurationUtilsSord = DurationUtilsSord1;
+          userName = test.Utils.getCurrentUserName();
+          test.Utils.getUserInfo(userName).then(function success3(userInfo1) {
+            userInfo = userInfo1;
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }, function error(err) {
+          fail(err);
+          console.error(err);
+          done();
+        }
+        );
+      }, function error(err) {
+        fail(err);
+        console.error(err);
+        done();
+      }
+      );
+    }).not.toThrow();
+  });
+  describe("Test Lib Functions", function () {
+    describe("sol.contract.DurationUtils", function () {
+      it("addUpdate", function (done) {
+        expect(function () {
+          tplSord = { objKeys: {} };
+          updates = { objKeys: {}, mapKeys: {} };
+          update = { type: "GRP", value: "value1" };
+          test.Utils.execute("RF_sol_unittest_contract_service_ExecuteLib", {
+            className: "sol.contract.DurationUtils",
+            classConfig: {},
+            method: "addUpdate",
+            params: [tplSord, updates, update]
+          }).then(function success(jsonResult) {
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("adjustToRealMonthEnd", function (done) {
+        expect(function () {
+          mom = "20190217";
+          endOf = "M";
+          test.Utils.execute("RF_sol_unittest_contract_service_ExecuteLib", {
+            className: "sol.contract.DurationUtils",
+            classConfig: {},
+            method: "adjustToRealMonthEnd",
+            params: [mom, endOf]
+          }).then(function success(jsonResult) {
+            expect(jsonResult).toEqual("2019-02-28T23:59:59.999Z");
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("calcContract", function (done) {
+        expect(function () {
+          tplSord = { objKeys: { CONTRACT_END: "20210101" }, mapKeys: { DURATION_TYPE: "sol.contract.form.permanent", REMINDER_PERIOD: 5, REMINDER_PERIOD_UNIT: "y" } };
+          updates = { objKeys: {}, mapKeys: {} };
+          test.Utils.execute("RF_sol_unittest_contract_service_ExecuteLib", {
+            className: "sol.contract.DurationUtils",
+            classConfig: {},
+            method: "calcContract",
+            params: [tplSord, updates]
+          }).then(function success(jsonResult) {
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("calcContractEnd", function (done) {
+        expect(function () {
+          tplSord = { objKeys: { CONTRACT_START: "20190101" }, mapKeys: { CONTRACT_DURATION: "1", CONTRACT_DURATION_UNIT: "y" } };
+          updates = { objKeys: {}, mapKeys: {} };
+          additionalShiftCounter = 0;
+          test.Utils.execute("RF_sol_unittest_contract_service_ExecuteLib", {
+            className: "sol.contract.DurationUtils",
+            classConfig: {},
+            method: "calcContractEnd",
+            params: [tplSord, updates, additionalShiftCounter]
+          }).then(function success(jsonResult) {
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("calcDuration", function (done) {
+        expect(function () {
+          startDateIso = "20190101";
+          endDateIso = "20220302";
+          unit = "d";
+          test.Utils.execute("RF_sol_unittest_contract_service_ExecuteLib", {
+            className: "sol.contract.DurationUtils",
+            classConfig: {},
+            method: "calcDuration",
+            params: [startDateIso, endDateIso, unit]
+          }).then(function success(jsonResult) {
+            expect(jsonResult).toEqual(1156);
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("calcLocalCurrencyAmount", function (done) {
+        expect(function () {
+          amount = 5;
+          exchangeRate = 2;
+          test.Utils.execute("RF_sol_unittest_contract_service_ExecuteLib", {
+            className: "sol.contract.DurationUtils",
+            classConfig: {},
+            method: "calcLocalCurrencyAmount",
+            params: [amount, exchangeRate]
+          }).then(function success(jsonResult) {
+            expect(jsonResult).toEqual(2.5);
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("calcNextPossibleContractEnd", function (done) {
+        expect(function () {
+          tplSord = { objKeys: { CONTRACT_END: "20210101" }, mapKeys: { DURATION_TYPE: "" } };
+          updates = { objKeys: {}, mapKeys: {} };
+          additionalShiftCounter = 0;
+          test.Utils.execute("RF_sol_unittest_contract_service_ExecuteLib", {
+            className: "sol.contract.DurationUtils",
+            classConfig: {},
+            method: "calcNextPossibleContractEnd",
+            params: [tplSord, updates, additionalShiftCounter]
+          }).then(function success(jsonResult) {
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("calcReminderDate", function (done) {
+        expect(function () {
+          tplSord = { objKeys: { CONTRACT_START: "20190101" }, mapKeys: { CONTRACT_DURATION: "1", CONTRACT_DURATION_UNIT: "y", NEXT_POSSIBLE_CONTRACT_END: "" } };
+          updates = { objKeys: {}, mapKeys: {} };
+          test.Utils.execute("RF_sol_unittest_contract_service_ExecuteLib", {
+            className: "sol.contract.DurationUtils",
+            classConfig: {},
+            method: "calcReminderDate",
+            params: [tplSord, updates]
+          }).then(function success(jsonResult) {
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("calcRowTotal", function (done) {
+        expect(function () {
+          startDateIso = "20190101";
+          endDateIso = "20220302";
+          singleAmount = 2;
+          unit = "d";
+          test.Utils.execute("RF_sol_unittest_contract_service_ExecuteLib", {
+            className: "sol.contract.DurationUtils",
+            classConfig: {},
+            method: "calcRowTotal",
+            params: [startDateIso, endDateIso, singleAmount, unit]
+          }).then(function success(jsonResult) {
+            expect(jsonResult).toEqual("2312");
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("calcTerminationDateFixed", function (done) {
+        expect(function () {
+          tplSord = { objKeys:{ CONTRACT_END: "20210101" }, mapKeys: { NOTICE_PERIOD: 5, NOTICE_PERIOD_UNIT: "y" } };
+          updates = { objKeys: {}, mapKeys: {} };
+          test.Utils.execute("RF_sol_unittest_contract_service_ExecuteLib", {
+            className: "sol.contract.DurationUtils",
+            classConfig: {},
+            method: "calcTerminationDateFixed",
+            params: [tplSord, updates]
+          }).then(function success(jsonResult) {
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("calcTerminationDateFromNow", function (done) {
+        expect(function () {
+          tplSord = { objKeys: { NEXT_POSSIBLE_TERMINATION: "20190101" }, mapKeys: { CONTRACT_DURATION: "1", CONTRACT_DURATION_UNIT: "M", NOTICE_PERIOD: 5, NOTICE_PERIOD_UNIT: "y", NEXT_POSSIBLE_CONTRACT_END: "20210101" } };
+          updates = { objKeys: {}, mapKeys: {} };
+          test.Utils.execute("RF_sol_unittest_contract_service_ExecuteLib", {
+            className: "sol.contract.DurationUtils",
+            classConfig: {},
+            method: "calcTerminationDateFromNow",
+            params: [tplSord, updates]
+          }).then(function success(jsonResult) {
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("clearContractEnd", function (done) {
+        expect(function () {
+          tplSord = { objKeys: { CONTRACT_START: "20190101" } };
+          test.Utils.execute("RF_sol_unittest_contract_service_ExecuteLib", {
+            className: "sol.contract.DurationUtils",
+            classConfig: {},
+            method: "clearContractEnd",
+            params: [tplSord]
+          }).then(function success(jsonResult) {
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("extendContract", function (done) {
+        expect(function () {
+          tplSord = { objKeys: { CONTRACT_END: "20210101" }, mapKeys: { DURATION_TYPE: "sol.contract.form.permanent", REMINDER_PERIOD: 5, REMINDER_PERIOD_UNIT: "y" } };
+          updates = { objKeys: {}, mapKeys: {} };
+          test.Utils.execute("RF_sol_unittest_contract_service_ExecuteLib", {
+            className: "sol.contract.DurationUtils",
+            classConfig: {},
+            method: "extendContract",
+            params: [tplSord, updates]
+          }).then(function success(jsonResult) {
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      xit("extendFixedTermContract", function (done) {
+        expect(function () {
+          tplSord = PVALUE;
+          updates = { objKeys: {}, mapKeys: {} };
+          test.Utils.execute("RF_sol_unittest_contract_service_ExecuteLib", {
+            className: "sol.contract.DurationUtils",
+            classConfig: {},
+            method: "extendFixedTermContract",
+            params: [tplSord, updates]
+          }).then(function success(jsonResult) {
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      xit("getBaseCurrency", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_unittest_contract_service_ExecuteLib", {
+            className: "sol.contract.DurationUtils",
+            classConfig: {},
+            method: "getBaseCurrency",
+            params: []
+          }).then(function success(jsonResult) {
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      xit("getEndOfDate", function (done) {
+        expect(function () {
+          date = PVALUE;
+          unit = PVALUE;
+          test.Utils.execute("RF_sol_unittest_contract_service_ExecuteLib", {
+            className: "sol.contract.DurationUtils",
+            classConfig: {},
+            method: "getEndOfDate",
+            params: [date, unit]
+          }).then(function success(jsonResult) {
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      xit("getKwlKey", function (done) {
+        expect(function () {
+          str = PVALUE;
+          separator = PVALUE;
+          test.Utils.execute("RF_sol_unittest_contract_service_ExecuteLib", {
+            className: "sol.contract.DurationUtils",
+            classConfig: {},
+            method: "getKwlKey",
+            params: [str, separator]
+          }).then(function success(jsonResult) {
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      xit("getSmallestUnitString", function (done) {
+        expect(function () {
+          unitStrings = PVALUE;
+          test.Utils.execute("RF_sol_unittest_contract_service_ExecuteLib", {
+            className: "sol.contract.DurationUtils",
+            classConfig: {},
+            method: "getSmallestUnitString",
+            params: [unitStrings]
+          }).then(function success(jsonResult) {
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      xit("isoToMoment", function (done) {
+        expect(function () {
+          isoDate = PVALUE;
+          test.Utils.execute("RF_sol_unittest_contract_service_ExecuteLib", {
+            className: "sol.contract.DurationUtils",
+            classConfig: {},
+            method: "isoToMoment",
+            params: [isoDate]
+          }).then(function success(jsonResult) {
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      xit("momentToIso", function (done) {
+        expect(function () {
+          mom = PVALUE;
+          test.Utils.execute("RF_sol_unittest_contract_service_ExecuteLib", {
+            className: "sol.contract.DurationUtils",
+            classConfig: {},
+            method: "momentToIso",
+            params: [mom]
+          }).then(function success(jsonResult) {
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+    });
+  });
+  afterAll(function (done) {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    expect(function () {
+      test.Utils.getTempfolder().then(function success(tempfolder) {
+        test.Utils.deleteSord(tempfolder).then(function success1(deleteResult) {
+          test.Utils.getFinishedWorkflows().then(function success2(wfs) {
+            test.Utils.removeFinishedWorkflows(wfs).then(function success3(removeFinishedWorkflowsResult) {
+              done();
+            }, function error(err) {
+              fail(err);
+              console.error(err);
+              done();
+            }
+            );
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }, function error(err) {
+          fail(err);
+          console.error(err);
+          done();
+        }
+        );
+      }, function error(err) {
+        fail(err);
+        console.error(err);
+        done();
+      }
+      );
+    }).not.toThrow();
+  });
+});
