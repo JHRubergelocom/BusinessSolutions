2559e9bc02907899bcbf0d87886104d091f27059
Jan-Hendrik Ruberg <j-h.ruberg@elo.com>
2019-11-21 14:31:42 +0100

    UnitTests/productivity/[service] sol.privacy.ix.services.ProcessingActivityPreconditions.js

---
+++ UnitTests/productivity/[service] sol.privacy.ix.services.ProcessingActivityPreconditions.js
@@ -0,0 +1,87 @@
+
+describe("[service] sol.privacy.ix.services.ProcessingActivityPreconditions", function () {
+  var ProcessActivitySord, originalTimeout;
+
+  beforeAll(function (done) {
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
+    expect(function () {
+      test.Utils.getSord("ARCPATH:/Administration/Business Solutions/productivity [unit tests]/Test data/ProcessActivity").then(function success(ProcessActivitySord1) {
+        ProcessActivitySord = ProcessActivitySord1;
+        done();
+      }, function error(err) {
+        fail(err);
+        console.error(err);
+        done();
+      }
+      );
+    }).not.toThrow();
+  });
+  describe("Tests Registered Functions", function () {
+    describe("RF_sol_privacy_service_ApproveProcessingActivityPreconditions", function () {
+      it("should throw if executed without 'targetId'", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_privacy_service_ApproveProcessingActivityPreconditions", {
+          }).then(function success(jsonResult) {
+            fail(jsonResult);
+            done();
+          }, function error(err) {
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("with 'targetId'", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_privacy_service_ApproveProcessingActivityPreconditions", {
+            targetId: ProcessActivitySord.id
+          }).then(function success(jsonResult) {
+            expect(jsonResult.valid).toBeDefined();
+            expect(jsonResult.valid).toEqual(true);
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+    });
+    describe("RF_sol_privacy_service_DelegateProcessingActivityPreconditions", function () {
+      it("should throw if executed without 'targetId'", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_privacy_service_DelegateProcessingActivityPreconditions", {
+          }).then(function success(jsonResult) {
+            fail(jsonResult);
+            done();
+          }, function error(err) {
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("with 'targetId'", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_privacy_service_DelegateProcessingActivityPreconditions", {
+            targetId: ProcessActivitySord.id
+          }).then(function success(jsonResult) {
+            expect(jsonResult.valid).toBeDefined();
+            expect(jsonResult.valid).toEqual(true);
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+    });
+  });
+  afterAll(function () {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+  });
+});
