69f7eab30692612cf5adacab701440b9e1596ffb
Jan-Hendrik Ruberg <j-h.ruberg@elo.com>
2019-12-16 14:59:03 +0100

    UnitTests/common/[libas] sol.unittest.as.services.SolCommonRendererFop.js

---
+++ UnitTests/common/[libas] sol.unittest.as.services.SolCommonRendererFop.js
@@ -0,0 +1,117 @@
+
+describe("[libas] sol.unittest.as.services.SolCommonRendererFop", function () {
+  var RendererFopSord, obSolCommonRendererFopId, originalTimeout, content, config, name, data;
+
+  beforeAll(function (done) {
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
+    expect(function () {
+      test.Utils.createTempSord("SolCommonRendererFop").then(function success(obSolCommonRendererFopId1) {
+        obSolCommonRendererFopId = obSolCommonRendererFopId1;
+        test.Utils.getSord("ARCPATH:/Administration/Business Solutions/common [unit tests]/Resources/RendererFop").then(function success1(RendererFopSord1) {
+          RendererFopSord = RendererFopSord1;
+          done();
+        }, function error(err) {
+          fail(err);
+          console.error(err);
+          done();
+        }
+        );
+      }, function error(err) {
+        fail(err);
+        console.error(err);
+        done();
+      }
+      );
+    }).not.toThrow();
+  });
+  describe("Test Lib Functions", function () {
+    describe("sol.common.as.renderer.Fop", function () {
+      it("initialize", function (done) {
+        expect(function () {
+          config = {};
+          test.Utils.execute("RF_sol_common_service_ExecuteAsAction", {
+            action: "sol.unittest.as.services.ExecuteLib",
+            config: {
+              className: "sol.common.as.renderer.Fop",
+              classConfig: { targetId: obSolCommonRendererFopId, templateId: RendererFopSord.id },
+              method: "initialize",
+              params: [config]
+            }
+          }).then(function success(jsonResult) {
+            content = jsonResult.content;
+            if (content.indexOf("exception") != -1) {
+              fail(jsonResult.content);
+            }
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("render", function (done) {
+        expect(function () {
+          name = "name1";
+          data = {};
+          test.Utils.execute("RF_sol_common_service_ExecuteAsAction", {
+            action: "sol.unittest.as.services.ExecuteLib",
+            config: {
+              className: "sol.common.as.renderer.Fop",
+              classConfig: { targetId: obSolCommonRendererFopId, templateId: RendererFopSord.id },
+              method: "render",
+              params: [name, data]
+            }
+          }).then(function success(jsonResult) {
+            content = jsonResult.content;
+            if (content.indexOf("exception") != -1) {
+              fail(jsonResult.content);
+            }
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+    });
+  });
+  afterAll(function (done) {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    expect(function () {
+      test.Utils.getTempfolder().then(function success(tempfolder) {
+        test.Utils.deleteSord(tempfolder).then(function success1(deleteResult) {
+          test.Utils.getFinishedWorkflows().then(function success2(wfs) {
+            test.Utils.removeFinishedWorkflows(wfs).then(function success3(removeFinishedWorkflowsResult) {
+              done();
+            }, function error(err) {
+              fail(err);
+              console.error(err);
+              done();
+            }
+            );
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }, function error(err) {
+          fail(err);
+          console.error(err);
+          done();
+        }
+        );
+      }, function error(err) {
+        fail(err);
+        console.error(err);
+        done();
+      }
+      );
+    }).not.toThrow();
+  });
+});
