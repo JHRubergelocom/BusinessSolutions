5701886fd3e0d26c774efee1139167254c399a08
Jan-Hendrik Ruberg <j-h.ruberg@elo.com>
2020-01-07 15:37:12 +0100

    UnitTests/common/[libas] sol.unittest.as.services.SolCommonMonitoringAnalyzersValueAnalyzer.js

--- UnitTests/common/lib_sol.unittest.as.services.ExecuteLib1.js
+++ UnitTests/common/lib_sol.unittest.as.services.ExecuteLib1.js
@@ -95,6 +95,19 @@ sol.define("sol.unittest.as.services.ExecuteLib1", {
           default:
         }
         break;
+      case "sol.common_monitoring.as.analyzers.ValueAnalyzer":
+        switch (me.method) {
+          case "analyze":
+          case "checkReferenceValues":
+          case "exists":
+          case "getValue":
+          case "isEmpty":
+          case "notEmpty":
+            me.params[0] = ixConnect.ix().checkoutSord(me.params[0], new SordZ(SordC.mbAll), LockC.NO);
+            break;
+          default:
+        }
+        break;
       default:
     }
 
