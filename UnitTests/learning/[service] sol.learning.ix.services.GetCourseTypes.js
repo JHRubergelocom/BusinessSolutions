53a7e28c154698cecb1256dbc73760baa4d81b10
Jan-Hendrik Ruberg <J-H.Ruberg@elo.com>
2020-04-16 14:31:42 +0200

    UnitTests/learning/[service] sol.learning.ix.services.GetCourseTypes.js

---
+++ UnitTests/learning/[service] sol.learning.ix.services.GetCourseTypes.js
@@ -0,0 +1,73 @@
+
+describe("[service] sol.learning.ix.services.GetCourseTypes", function () {
+  var originalTimeout;
+
+  beforeAll(function (done) {
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
+    expect(function () {
+      test.Utils.createTempSord("GetCourseTypes").then(function success(objTempId) {
+        done();
+      }, function error(err) {
+        fail(err);
+        console.error(err);
+        done();
+      }
+      );
+    }).not.toThrow();
+  });
+  describe("Tests Registered Functions", function () {
+    describe("RF_sol_learning_service_GetCourseTypeInfos", function () {
+      it("should throw if executed without parameters ", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_learning_service_GetCourseTypeInfos", {
+          }).then(function success(jsonResult) {
+            expect(jsonResult).toBeDefined();
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+    });
+    describe("RF_sol_learning_service_GetCourseTypes", function () {
+      it("should not throw if executed without parameters ", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_learning_service_GetCourseTypes", {
+          }).then(function success(jsonResult) {
+            expect(jsonResult).toBeDefined();
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+    });
+  });
+  afterAll(function (done) {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    expect(function () {
+      test.Utils.getTempfolder().then(function success(tempfolder) {
+        test.Utils.deleteSord(tempfolder).then(function success1(deleteResult) {
+          done();
+        }, function error(err) {
+          fail(err);
+          console.error(err);
+          done();
+        }
+        );
+      }, function error(err) {
+        fail(err);
+        console.error(err);
+        done();
+      }
+      );
+    }).not.toThrow();
+  });
+});
