1bfbacc51fe2148a59be7a55fc022a0cbcbbceff
Jan-Hendrik Ruberg <J-H.Ruberg@elo.com>
2020-07-08 11:26:25 +0200

    UnitTests/recruitung/[function] sol.recruiting.ix.functions.GetRedirectRequest.js

---
+++ UnitTests/recruitung/[function] sol.recruiting.ix.functions.GetRedirectRequest.js
@@ -0,0 +1,57 @@
+
+describe("[function] sol.recruiting.ix.functions.GetRedirectRequest", function () {
+  var originalTimeout;
+
+  beforeAll(function (done) {
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
+    expect(function () {
+      test.Utils.createTempSord("GetRedirectRequest").then(function success(objTempId) {
+        done();
+      }, function error(err) {
+        fail(err);
+        console.error(err);
+        done();
+      }
+      );
+    }).not.toThrow();
+  });
+  describe("Tests Registered Functions", function () {
+    describe("RF_sol_recruiting_function_GetRedirectRequest", function () {
+      it("should not throw if executed without Parameter", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_recruiting_function_GetRedirectRequest", {
+          }).then(function success(jsonResult) {
+            expect(jsonResult).toBeDefined();
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+    });
+  });
+  afterAll(function (done) {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    expect(function () {
+      test.Utils.getTempfolder().then(function success(tempfolder) {
+        test.Utils.deleteSord(tempfolder).then(function success1(deleteResult) {
+          done();
+        }, function error(err) {
+          fail(err);
+          console.error(err);
+          done();
+        }
+        );
+      }, function error(err) {
+        fail(err);
+        console.error(err);
+        done();
+      }
+      );
+    }).not.toThrow();
+  });
+});
