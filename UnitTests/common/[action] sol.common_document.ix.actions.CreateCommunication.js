9e5d2792ca532d0c97e3dde6ff691d5f9e9e61ae
Jan-Hendrik Ruberg <J-H.Ruberg@elo.com>
2020-07-29 11:13:00 +0200

    UnitTests/common/[action] sol.common_document.ix.actions.CreateCommunication.js

--- UnitTests/common/[action] sol.common_document.ix.actions.CreateCommunication.js
+++ UnitTests/common/[action] sol.common_document.ix.actions.CreateCommunication.js
@@ -165,7 +165,7 @@ describe("[action] sol.common_document.ix.actions.CreateCommunication", function
     it("finish input forwarding workflow", function (done) {
       expect(function () {
         test.Utils.getWorkflow(wfInfo.flowId).then(function success(workflow) {
-          succNodes = test.Utils.getSuccessorNodes(workflow, wfInfo.nodeId, null, "sol.common_document.communication.create.node.send");
+          succNodes = test.Utils.getSuccessorNodes(workflow, wfInfo.nodeId, null, "sol.common.wf.node.ok");
           succNodesIds = test.Utils.getSuccessorNodesIds(succNodes);
           test.Utils.forwardWorkflowTask(wfInfo.flowId, wfInfo.nodeId, succNodesIds, "Unittest finish input").then(function success1(forwardWorkflowTaskResult) {
             done();
