48c1b61683daf7d36959444f4b3f2a0c999594f3
Jan-Hendrik Ruberg <J-H.Ruberg@elo.com>
2020-10-14 10:33:41 +0200

    UnitTests/common/[service] sol.dev.ix.services.GetObjIDbyMD5.js

---
+++ UnitTests/common/[service] sol.dev.ix.services.GetObjIDbyMD5.js
@@ -0,0 +1,103 @@
+
+describe("[service] sol.dev.ix.services.GetObjIDbyMD5", function () {
+  var originalTimeout, md5;
+
+  beforeAll(function (done) {
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
+    expect(function () {
+      test.Utils.createTempSord("GetObjIDbyMD5").then(function success(objTempId) {
+        test.Utils.getSord("ARCPATH:/Administration/Business Solutions/common [unit tests]/Resources/Smiley1").then(function success1(smiley1Sord) {
+          test.Utils.getCurrentWorkVersion(smiley1Sord.id).then(function success2(dv) {
+            md5 = dv.md5;
+            expect(md5).toBeDefined();
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }, function error(err) {
+          fail(err);
+          console.error(err);
+          done();
+        }
+        );
+      }, function error(err) {
+        fail(err);
+        console.error(err);
+        done();
+      }
+      );
+    }).not.toThrow();
+  });
+  describe("Tests Registered Functions", function () {
+    describe("RF_sol_dev_service_GetObjIDbyMD5", function () {
+      it("should throw if executed without 'md5'", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_dev_service_GetObjIDbyMD5", {
+          }).then(function success(jsonResult) {
+            fail(jsonResult);
+            done();
+          }, function error(err) {
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("GetObjIDbyMD5", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_dev_service_GetObjIDbyMD5", {
+            md5: "'" + md5 + "'"
+          }).then(function success(jsonResult) {
+            expect(jsonResult).toBeDefined();
+            expect(jsonResult.length).toBeGreaterThan(0);
+            expect(jsonResult[0].objid).toBeDefined();
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+    });
+  });
+  afterAll(function (done) {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    expect(function () {
+      test.Utils.getTempfolder().then(function success(tempfolder) {
+        test.Utils.deleteSord(tempfolder).then(function success1(deleteResult) {
+          test.Utils.getFinishedWorkflows().then(function success2(wfs) {
+            test.Utils.removeFinishedWorkflows(wfs).then(function success3(removeFinishedWorkflowsResult) {
+              done();
+            }, function error(err) {
+              fail(err);
+              console.error(err);
+              done();
+            }
+            );
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }, function error(err) {
+          fail(err);
+          console.error(err);
+          done();
+        }
+        );
+      }, function error(err) {
+        fail(err);
+        console.error(err);
+        done();
+      }
+      );
+    }).not.toThrow();
+  });
+});
