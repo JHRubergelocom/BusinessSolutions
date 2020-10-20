cd13c4bcd18c1039e344930b26d87f5abb7a27fc
Jan-Hendrik Ruberg <J-H.Ruberg@elo.com>
2020-10-20 15:56:31 +0200

    UnitTests/contract/[dynkwl] sol.contract.ix.dynkwls.ContactList.js

---
+++ UnitTests/contract/[dynkwl] sol.contract.ix.dynkwls.ContractPartnerAdditional.js
@@ -0,0 +1,145 @@
+
+describe("[dynkwl] sol.contract.ix.dynkwls.ContractPartnerAdditional", function () {
+  var originalTimeout, objContractPartnerAdditionalId, kwlConfig;
+
+  beforeAll(function (done) {
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
+    expect(function () {
+      test.Utils.createTempSord("ContractPartnerAdditional").then(function success(objContractPartnerAdditionalId1) {
+        objContractPartnerAdditionalId = objContractPartnerAdditionalId1;
+        done();
+      }, function error(err) {
+        fail(err);
+        console.error(err);
+        done();
+      }
+      );
+    }).not.toThrow();
+  });
+  describe("Tests Dynamic Keyword lists", function () {
+    describe("sol.contract.ix.dynkwl.ContractPartnerAdditional", function () {
+      it("get 'Additional contract partners' configuration from kwl.config", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_common_service_GetConfig", {
+            objId: "ARCPATH[(E10E1000-E100-E100-E100-E10E10E10E00)]:/Business Solutions/contract/Configuration/kwl.config"
+          }).then(function success(configResult) {
+            kwlConfig = configResult.config["Additional contract partners"];
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("kwlConfig must be available", function () {
+        expect(kwlConfig).toBeDefined();
+      });
+      it("should throw if executed without 'objId'", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_unittest_contract_service_ExecuteDynKwl", {
+          }).then(function success(jsonResult) {
+            fail(jsonResult);
+            done();
+          }, function error(err) {
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("should throw if executed without 'dynkwl'", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_unittest_contract_service_ExecuteDynKwl", {
+            objId: objContractPartnerAdditionalId
+          }).then(function success(jsonResult) {
+            fail(jsonResult);
+            done();
+          }, function error(err) {
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("should throw if executed without 'providerConfig'", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_unittest_contract_service_ExecuteDynKwl", {
+            objId: objContractPartnerAdditionalId,
+            dynKwl: "sol.common.ix.GenericDynKwl"
+          }).then(function success(jsonResult) {
+            fail(jsonResult);
+            done();
+          }, function error(err) {
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("should throw if executed without 'inputFieldName'", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_unittest_contract_service_ExecuteDynKwl", {
+            objId: objContractPartnerAdditionalId,
+            dynKwl: "sol.common.ix.GenericDynKwl",
+            providerConfig: kwlConfig
+          }).then(function success(jsonResult) {
+            fail(jsonResult);
+            done();
+          }, function error(err) {
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("should not throw", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_unittest_contract_service_ExecuteDynKwl", {
+            objId: objContractPartnerAdditionalId,
+            dynKwl: "sol.common.ix.GenericDynKwl",
+            providerConfig: kwlConfig,
+            inputFieldName: "UNITTEST_FIELD2"
+          }).then(function success(jsonResult) {   
+            if (jsonResult.error) {
+              fail(jsonResult.error);
+            } else {
+              expect(jsonResult.keynames).toBeDefined();
+              expect(jsonResult.header).toBeDefined();
+              expect(jsonResult.title).toBeDefined();
+              expect(jsonResult.data).toBeDefined();  
+            }
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+    });
+  });
+  afterAll(function (done) {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    expect(function () {
+      test.Utils.getTempfolder().then(function success(tempfolder) {
+        test.Utils.deleteSord(tempfolder).then(function success1(deleteResult) {
+          done();
+        }, function error(err) {
+          fail(err);
+          console.error(err);
+          done();
+        }
+        );
+      }, function error(err) {
+        fail(err);
+        console.error(err);
+        done();
+      }
+      );
+    }).not.toThrow();
+  });
+});
