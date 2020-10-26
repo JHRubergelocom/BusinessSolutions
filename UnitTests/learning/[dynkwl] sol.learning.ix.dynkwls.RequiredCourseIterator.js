46043f9398ca09931f3999065c45a697b29833ea
Jan-Hendrik Ruberg <J-H.Ruberg@elo.com>
2020-10-26 13:56:40 +0100

    UnitTests/learning/[dynkwl] sol.learning.ix.dynkwls.CourseIterator.js

---
+++ UnitTests/learning/[dynkwl] sol.learning.ix.dynkwls.RequiredCourseIterator.js
@@ -0,0 +1,127 @@
+
+describe("[dynkwl] sol.learning.ix.dynkwls.RequiredCourseIterator", function () {
+  var originalTimeout, objRequiredCourseIteratorId;
+
+  beforeAll(function (done) {
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
+    expect(function () {
+      test.Utils.createTempSord("RequiredCourseIterator").then(function success(objRequiredCourseIteratorId1) {
+        objRequiredCourseIteratorId = objRequiredCourseIteratorId1;
+        done();
+      }, function error(err) {
+        fail(err);
+        console.error(err);
+        done();
+      }
+      );
+    }).not.toThrow();
+  });
+  describe("Tests Dynamic Keyword lists", function () {
+    describe("sol.learning.ix.dynkwl.RequiredCourseIterator", function () {
+      it("should throw if executed without 'objId'", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_unittest_learning_service_ExecuteDynKwl", {
+          }).then(function success(jsonResult) {
+            fail(jsonResult);
+            done();
+          }, function error(err) {
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("should throw if executed without 'dynkwl'", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_unittest_learning_service_ExecuteDynKwl", {
+            objId: objRequiredCourseIteratorId
+          }).then(function success(jsonResult) {
+            fail(jsonResult);
+            done();
+          }, function error(err) {
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("should throw if executed without 'providerConfig'", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_unittest_learning_service_ExecuteDynKwl", {
+            objId: objRequiredCourseIteratorId,
+            dynKwl: "sol.learning.ix.dynkwl.RequiredCourseIterator"
+          }).then(function success(jsonResult) {
+            fail(jsonResult);
+            done();
+          }, function error(err) {
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("should throw if executed without 'inputFieldName'", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_unittest_learning_service_ExecuteDynKwl", {
+            objId: objRequiredCourseIteratorId,
+            dynKwl: "sol.learning.ix.dynkwl.RequiredCourseIterator",
+            providerConfig: {}
+          }).then(function success(jsonResult) {
+            fail(jsonResult);
+            done();
+          }, function error(err) {
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("should not throw", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_unittest_learning_service_ExecuteDynKwl", {
+            objId: objRequiredCourseIteratorId,
+            dynKwl: "sol.learning.ix.dynkwl.RequiredCourseIterator",
+            providerConfig: {},
+            inputFieldName: "UNITTEST_FIELD2"
+          }).then(function success(jsonResult) {   
+            if (jsonResult.error) {
+              fail(jsonResult.error);
+            } else {
+              expect(jsonResult.keynames).toBeDefined();
+              expect(jsonResult.header).toBeDefined();
+              expect(jsonResult.title).toBeDefined();
+              expect(jsonResult.data).toBeDefined();  
+            }
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+    });
+  });
+  afterAll(function (done) {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    expect(function () {
+      test.Utils.getTempfolder().then(function success(tempfolder) {
+        test.Utils.deleteSord(tempfolder).then(function success1(deleteResult) {
+          done();
+        }, function error(err) {
+          fail(err);
+          console.error(err);
+          done();
+        }
+        );
+      }, function error(err) {
+        fail(err);
+        console.error(err);
+        done();
+      }
+      );
+    }).not.toThrow();
+  });
+});
---
+++ UnitTests/learning/[dynkwl] sol.learning.ix.dynkwls.generators.SessionNo.js
@@ -0,0 +1,127 @@
+
+describe("[dynkwl] sol.learning.ix.dynkwls.generators.SessionNo", function () {
+  var originalTimeout, objGeneratorsSessionNoId;
+
+  beforeAll(function (done) {
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
+    expect(function () {
+      test.Utils.createTempSord("GeneratorsSessionNo").then(function success(objGeneratorsSessionNoId1) {
+        objGeneratorsSessionNoId = objGeneratorsSessionNoId1;
+        done();
+      }, function error(err) {
+        fail(err);
+        console.error(err);
+        done();
+      }
+      );
+    }).not.toThrow();
+  });
+  describe("Tests Dynamic Keyword lists", function () {
+    describe("sol.learning.ix.dynkwl.generators.SessionNo", function () {
+      it("should throw if executed without 'objId'", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_unittest_learning_service_ExecuteDynKwl", {
+          }).then(function success(jsonResult) {
+            fail(jsonResult);
+            done();
+          }, function error(err) {
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("should throw if executed without 'dynkwl'", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_unittest_learning_service_ExecuteDynKwl", {
+            objId: objGeneratorsSessionNoId
+          }).then(function success(jsonResult) {
+            fail(jsonResult);
+            done();
+          }, function error(err) {
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("should throw if executed without 'providerConfig'", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_unittest_learning_service_ExecuteDynKwl", {
+            objId: objGeneratorsSessionNoId,
+            dynKwl: "sol.learning.ix.dynkwl.generators.SessionNo"
+          }).then(function success(jsonResult) {
+            fail(jsonResult);
+            done();
+          }, function error(err) {
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("should throw if executed without 'inputFieldName'", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_unittest_learning_service_ExecuteDynKwl", {
+            objId: objGeneratorsSessionNoId,
+            dynKwl: "sol.learning.ix.dynkwl.generators.SessionNo",
+            providerConfig: {}
+          }).then(function success(jsonResult) {
+            fail(jsonResult);
+            done();
+          }, function error(err) {
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("should not throw", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_unittest_learning_service_ExecuteDynKwl", {
+            objId: objGeneratorsSessionNoId,
+            dynKwl: "sol.learning.ix.dynkwl.generators.SessionNo",
+            providerConfig: {},
+            inputFieldName: "UNITTEST_FIELD2"
+          }).then(function success(jsonResult) {   
+            if (jsonResult.error) {
+              fail(jsonResult.error);
+            } else {
+              expect(jsonResult.keynames).toBeDefined();
+              expect(jsonResult.header).toBeDefined();
+              expect(jsonResult.title).toBeDefined();
+              expect(jsonResult.data).toBeDefined();  
+            }
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+    });
+  });
+  afterAll(function (done) {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    expect(function () {
+      test.Utils.getTempfolder().then(function success(tempfolder) {
+        test.Utils.deleteSord(tempfolder).then(function success1(deleteResult) {
+          done();
+        }, function error(err) {
+          fail(err);
+          console.error(err);
+          done();
+        }
+        );
+      }, function error(err) {
+        fail(err);
+        console.error(err);
+        done();
+      }
+      );
+    }).not.toThrow();
+  });
+});
