a58b9283c20e9d53893a00f98f23229d36f41b2a
Jan-Hendrik Ruberg <J-H.Ruberg@elo.com>
2020-10-26 15:28:12 +0100

    UnitTests/productivity/[dynkwl] sol.contact.ix.dynkwls.FindCompanyIterator.js

---
+++ UnitTests/productivity/[dynkwl] sol.contact.ix.dynkwls.FormOfAddressTemplate.js
@@ -0,0 +1,127 @@
+
+describe("[dynkwl] sol.contact.ix.dynkwls.FormOfAddressTemplate", function () {
+  var originalTimeout, objFormOfAddressTemplateId;
+
+  beforeAll(function (done) {
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
+    expect(function () {
+      test.Utils.createTempSord("FormOfAddressTemplate").then(function success(objFormOfAddressTemplateId1) {
+        objFormOfAddressTemplateId = objFormOfAddressTemplateId1;
+        done();
+      }, function error(err) {
+        fail(err);
+        console.error(err);
+        done();
+      }
+      );
+    }).not.toThrow();
+  });
+  describe("Tests Dynamic Keyword lists", function () {
+    describe("sol.contact.ix.dynkwl.FormOfAddressTemplate", function () {
+      it("should throw if executed without 'objId'", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_unittest_productivity_service_ExecuteDynKwl", {
+          }).then(function success(jsonResult) {
+            fail(jsonResult);
+            done();
+          }, function error(err) {
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("should throw if executed without 'dynkwl'", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_unittest_productivity_service_ExecuteDynKwl", {
+            objId: objFormOfAddressTemplateId
+          }).then(function success(jsonResult) {
+            fail(jsonResult);
+            done();
+          }, function error(err) {
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("should throw if executed without 'providerConfig'", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_unittest_productivity_service_ExecuteDynKwl", {
+            objId: objFormOfAddressTemplateId,
+            dynKwl: "sol.contact.ix.dynkwl.FormOfAddressTemplate"
+          }).then(function success(jsonResult) {
+            fail(jsonResult);
+            done();
+          }, function error(err) {
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("should throw if executed without 'inputFieldName'", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_unittest_productivity_service_ExecuteDynKwl", {
+            objId: objFormOfAddressTemplateId,
+            dynKwl: "sol.contact.ix.dynkwl.FormOfAddressTemplate",
+            providerConfig: {}
+          }).then(function success(jsonResult) {
+            fail(jsonResult);
+            done();
+          }, function error(err) {
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("should not throw", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_unittest_productivity_service_ExecuteDynKwl", {
+            objId: objFormOfAddressTemplateId,
+            dynKwl: "sol.contact.ix.dynkwl.FormOfAddressTemplate",
+            providerConfig: {},
+            inputFieldName: "UNITTEST_FIELD2"
+          }).then(function success(jsonResult) {   
+            if (jsonResult.error) {
+              fail(jsonResult.error);
+            } else {
+              expect(jsonResult.keynames).toBeDefined();
+              expect(jsonResult.header).toBeDefined();
+              expect(jsonResult.title).toBeDefined();
+              expect(jsonResult.data).toBeDefined();  
+            }
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+    });
+  });
+  afterAll(function (done) {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    expect(function () {
+      test.Utils.getTempfolder().then(function success(tempfolder) {
+        test.Utils.deleteSord(tempfolder).then(function success1(deleteResult) {
+          done();
+        }, function error(err) {
+          fail(err);
+          console.error(err);
+          done();
+        }
+        );
+      }, function error(err) {
+        fail(err);
+        console.error(err);
+        done();
+      }
+      );
+    }).not.toThrow();
+  });
+});
