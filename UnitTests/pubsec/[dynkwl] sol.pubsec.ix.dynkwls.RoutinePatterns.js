6cde588759007d584980045d09019837bbafbf6c
Jan-Hendrik Ruberg <J-H.Ruberg@elo.com>
2020-10-27 15:46:14 +0100

    UnitTests/pubsec/[dynkwl] sol.pubsec.ix.dynkwls.AllowedProcessTypes.js

---
+++ UnitTests/pubsec/[dynkwl] sol.pubsec.ix.dynkwls.RoutinePatterns.js
@@ -0,0 +1,127 @@
+
+describe("[dynkwl] sol.pubsec.ix.dynkwls.RoutinePatterns", function () {
+  var originalTimeout, objRoutinePatternsId;
+
+  beforeAll(function (done) {
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
+    expect(function () {
+      test.Utils.createTempSord("RoutinePatterns").then(function success(objRoutinePatternsId1) {
+        objRoutinePatternsId = objRoutinePatternsId1;
+        done();
+      }, function error(err) {
+        fail(err);
+        console.error(err);
+        done();
+      }
+      );
+    }).not.toThrow();
+  });
+  describe("Tests Dynamic Keyword lists", function () {
+    describe("sol.pubsec.ix.dynkwl.RoutinePatterns", function () {
+      it("should throw if executed without 'objId'", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_unittest_pubsec_service_ExecuteDynKwl", {
+          }).then(function success(jsonResult) {
+            fail(jsonResult);
+            done();
+          }, function error(err) {
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("should throw if executed without 'dynkwl'", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_unittest_pubsec_service_ExecuteDynKwl", {
+            objId: objRoutinePatternsId
+          }).then(function success(jsonResult) {
+            fail(jsonResult);
+            done();
+          }, function error(err) {
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("should throw if executed without 'providerConfig'", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_unittest_pubsec_service_ExecuteDynKwl", {
+            objId: objRoutinePatternsId,
+            dynKwl: "sol.pubsec.ix.dynkwl.RoutinePatterns"
+          }).then(function success(jsonResult) {
+            fail(jsonResult);
+            done();
+          }, function error(err) {
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("should throw if executed without 'inputFieldName'", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_unittest_pubsec_service_ExecuteDynKwl", {
+            objId: objRoutinePatternsId,
+            dynKwl: "sol.pubsec.ix.dynkwl.RoutinePatterns",
+            providerConfig: {}
+          }).then(function success(jsonResult) {
+            fail(jsonResult);
+            done();
+          }, function error(err) {
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("should not throw", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_unittest_pubsec_service_ExecuteDynKwl", {
+            objId: objRoutinePatternsId,
+            dynKwl: "sol.pubsec.ix.dynkwl.RoutinePatterns",
+            providerConfig: {},
+            inputFieldName: "UNITTEST_FIELD2"
+          }).then(function success(jsonResult) {   
+            if (jsonResult.error) {
+              fail(jsonResult.error);
+            } else {
+              expect(jsonResult.keynames).toBeDefined();
+              expect(jsonResult.header).toBeDefined();
+              expect(jsonResult.title).toBeDefined();
+              expect(jsonResult.data).toBeDefined();  
+            }
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+    });
+  });
+  afterAll(function (done) {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    expect(function () {
+      test.Utils.getTempfolder().then(function success(tempfolder) {
+        test.Utils.deleteSord(tempfolder).then(function success1(deleteResult) {
+          done();
+        }, function error(err) {
+          fail(err);
+          console.error(err);
+          done();
+        }
+        );
+      }, function error(err) {
+        fail(err);
+        console.error(err);
+        done();
+      }
+      );
+    }).not.toThrow();
+  });
+});
