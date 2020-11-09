7c625f355f2b38771453935dda42c207555d8ff5
Jan-Hendrik Ruberg <J-H.Ruberg@elo.com>
2020-11-09 14:02:26 +0100

    UnitTests/datev_accounting/sol.unittest.datev.accounting.ix.services.ExecuteDynKwl.js

---
+++ UnitTests/datev_accounting/sol.unittest.datev.accounting.ix.services.ExecuteDynKwl.js
@@ -0,0 +1,142 @@
+
+importPackage(Packages.java.io);
+importPackage(Packages.de.elo.ix.client);
+
+//@include lib_Class.js
+//@include lib_sol.common.ix.ServiceBase.js
+//@include lib_sol.common.ix.GenericDynKwl.js
+//@include sol.datev.accounting.dynkwl.RestIteratorWLCodes.js
+//@include sol.datev.accounting.ix.localizedKwl.BusinessPartnerType.js
+//@include sol.invoice.ix.localizedKwl.DocType.js
+//@include sol.invoice.ix.localizedKwl.Status.js
+//@include sol.online.ecm.dynkwl.RestIteratorBusinessPartner.js
+//@include sol.online.ecm.dynkwl.RestIteratorCashDiscount.js
+//@include sol.online.ecm.dynkwl.RestIteratorClient.js
+//@include sol.online.ecm.dynkwl.RestIteratorCostCenters1.js
+//@include sol.online.ecm.dynkwl.RestIteratorCostCenters2.js
+//@include sol.online.ecm.dynkwl.RestIteratorCreditor.js
+//@include sol.online.ecm.dynkwl.RestIteratorDueInDays.js
+//@include sol.online.ecm.dynkwl.RestIteratorFiscalYear.js
+//@include sol.online.ecm.dynkwl.RestIteratorLedgerAccount.js
+//@include sol.online.ecm.dynkwl.RestIteratorVendorPostingKey.js
+
+var logger = sol.create("sol.Logger", { scope: "sol.unittest.datev.accounting.ix.services.ExecuteDynKwl" });
+
+/**
+ * Unittests of dynamic keyword list.
+ *
+ * Examples
+ *
+ *
+ *     sol.common.IxUtils.execute('RF_sol_unittest_datev_accounting_service_ExecuteDynKwl', {
+ *       objId: 5201,
+ *       dynKwl: 'sol.unittest.ix.dynkwl.UserNames',
+ *       providerConfig: {
+ *          tableTitle :'Benutzerinfos',
+ *          tableHeaders: ['ID', 'Name'],
+ *          userIdFieldName:"Benutzer-ID"
+ *       },
+ *       "inputFieldName": "UNITTEST_FIELD2"
+ *     });
+ *
+ * *
+ * @author JHR, ELO Digital Office GmbH
+ * @version 1.0
+ *
+ */
+sol.define("sol.unittest.datev.accounting.ix.services.ExecuteDynKwl", {
+  extend: "sol.common.ix.ServiceBase",
+
+  requiredConfig: ["objId", "dynKwl", "providerConfig", "inputFieldName"],
+
+  /**
+   * @cfg {String} objId Sord objId.
+   */
+
+  /**
+   * @cfg {String} dynKwl Dnynamic Keyword list script name.
+   */
+
+  /**
+   * @cfg {Object} providerConfig Provider Configuration.
+   */
+
+  /**
+   * @cfg {Object} inputFieldName Input field.
+   */
+
+  initialize: function (config) {
+    var me = this;
+    me.$super("sol.common.ix.ServiceBase", "initialize", [config]);
+  },
+
+  /**
+   * Call the dynamic keyword list and returns the result
+   * @return {String|Object} result of dynamic keyword list
+   */
+  process: function () {
+    var me = this,
+        result = {},
+        resultData = [],
+        provider, sord, map;
+
+    try {
+
+      if (me.dynKwl == "sol.common.ix.GenericDynKwl") {
+        provider = sol.create("sol.common.ix.GenericDynKwl", me.providerConfig).getProvider();
+      } else {
+        provider = sol.create(me.dynKwl, me.providerConfig);
+      }
+
+      if (me.dynKwl == "sol.datev.accounting.dynkwl.RestIteratorWLCodes") {
+        map = ixConnect.ix().checkoutMap(MapDomainC.DOMAIN_SORD, me.objId, null, LockC.NO);
+        provider.openMap(me.ec, map, me.inputFieldName)
+      } else {
+        sord = ixConnect.ix().checkoutSord(me.objId, new SordZ(SordC.mbAll), LockC.NO);
+        provider.open(me.ec, sord, me.inputFieldName);  
+      }
+
+      if (provider.getMessage()) {
+        result.message = provider.getMessage();
+        return result;
+      }
+
+      while (provider.hasMoreRows()) {
+        resultData.push(provider.getNextRow());
+      }
+
+      result.keynames = provider.getKeyNames();
+      result.header = provider.getHeader();
+      result.title = provider.getTitle();
+      result.data = resultData;
+      
+    } catch (ex) {
+      result.error = String(ex);
+    } finally {
+      if (provider && (sol.common.ObjectUtils.isFunction(provider.close))) {
+        provider.close();
+      }
+    }
+
+    return result;
+  }
+});
+
+
+/**
+ * @member sol.unittest.datev.accounting.ix.services.ExecuteDynKwl
+ * @method RF_sol_unittest_datev_accounting_service_ExecuteDynKwl
+ * @static
+ * @inheritdoc sol.unittest.ix.ServiceBase#RF_ServiceBaseName
+ */
+function RF_sol_unittest_datev_accounting_service_ExecuteDynKwl(ec, args) {
+  var params, service, result;
+  logger.enter("RF_sol_unittest_datev_accounting_service_ExecuteDynKwl", args);
+
+  params = sol.common.ix.RfUtils.parseAndCheckParams(ec, arguments.callee.name, args, "objId", "dynKwl", "providerConfig", "inputFieldName");
+  params.ec = ec;
+  service = sol.create("sol.unittest.datev.accounting.ix.services.ExecuteDynKwl", params);
+  result = service.process();
+  logger.exit("RF_sol_unittest_datev_accounting_service_ExecuteDynKwl", result);
+  return sol.common.JsonUtils.stringifyAll(result);
+}
