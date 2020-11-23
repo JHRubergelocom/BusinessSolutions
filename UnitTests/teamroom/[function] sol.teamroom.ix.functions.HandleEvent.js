622299400f985394ae354b2e4362cfeae43876bb
Jan-Hendrik Ruberg <J-H.Ruberg@elo.com>
2020-11-23 15:34:26 +0100

    Scripting/teamroom/sol.teamroom.ix.functions.HandleEvent.js

---
+++ UnitTests/teamroom/[function] sol.teamroom.ix.functions.HandleEvent.js
@@ -0,0 +1,75 @@
+
+describe("[function] sol.teamroom.ix.functions.HandleEvent", function () {
+  var originalTimeout;
+
+  beforeAll(function (done) {
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
+    expect(function () {
+      test.Utils.createTempSord("HandleEvent").then(function success(objTempId) {
+        done();
+      }, function error(err) {
+        fail(err);
+        console.error(err);
+        done();
+      }
+      );
+    }).not.toThrow();
+  });
+  describe("Tests Registered Functions", function () {
+    describe("RF_sol_teamroom_function_HandleEvent", function () {
+      it("should throw if executed without paramter", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_teamroom_function_HandleEvent", {
+          }).then(function success(jsonResult) {
+            fail(jsonResult);
+            done();
+          }, function error(err) {
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("remove workflows", function (done) {
+        expect(function () {
+          test.Utils.getFinishedWorkflows().then(function success(wfs) {
+            test.Utils.removeFinishedWorkflows(wfs).then(function success1(removeFinishedWorkflowsResult) {
+              done();
+            }, function error(err) {
+              fail(err);
+              console.error(err);
+              done();
+            }
+            );
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+    });
+  });
+  afterAll(function (done) {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    expect(function () {
+      test.Utils.getTempfolder().then(function success(tempfolder) {
+        test.Utils.deleteSord(tempfolder).then(function success1(deleteResult) {
+          done();
+        }, function error(err) {
+          fail(err);
+          console.error(err);
+          done();
+        }
+        );
+      }, function error(err) {
+        fail(err);
+        console.error(err);
+        done();
+      }
+      );
+    }).not.toThrow();
+  });
+});
