6ba5b7786ac98e43c7b9cd2676ed65ed6cad9e45
Jan-Hendrik Ruberg <J-H.Ruberg@elo.com>
2021-05-27 15:49:16 +0200

    UnitTests/learning/[dynkwl] sol.learning.ix.dynkwls.BaseCourseIterator.js

---
+++ UnitTests/learning/[function] sol.learning.ix.functions.CreateRepetitionEnrollment.js
@@ -0,0 +1,172 @@
+
+describe("[function] sol.learning.ix.functions.CreateRepetitionEnrollment", function () {
+  var objIdEnr, objIdEnrRep, objIdCourse, originalTimeout, interval,
+      courseRef, coursePath, enrollmentPath;
+
+  beforeAll(function (done) {
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
+    expect(function () {
+      test.Utils.createTempSord("CreateRepetitionEnrollment").then(function success(objTempId) {
+        interval = 4000;
+        coursePath = "ARCPATH[(E10E1000-E100-E100-E100-E10E10E10E00)]:/Business Solutions/learning [unit tests]/Test data/Business Logic Provider";
+        test.Utils.getSord(coursePath).then(function success1(sordCourse) {
+          courseRef = test.Utils.getObjKeyValue(sordCourse, "COURSE_REFERENCE");
+          objIdCourse = sordCourse.id;
+          enrollmentPath = "ARCPATH[(E10E1000-E100-E100-E100-E10E10E10E00)]:/Business Solutions/learning [unit tests]/Test data/Business Logic Provider/Enrollments/Administrator";
+          test.Utils.getSord(enrollmentPath).then(function success2(sordEnr) {
+            objIdEnr = sordEnr.id;
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }, function error(err) {
+          fail(err);
+          console.error(err);
+          done();
+        }
+        );
+      }, function error(err) {
+        fail(err);
+        console.error(err);
+        done();
+      }
+      );
+    }).not.toThrow();
+  });
+  describe("Tests Registered Functions", function () {
+    describe("RF_sol_learning_function_CreateRepetitionEnrollment", function () {
+      it("set course repetition reference in course", function (done) {
+        expect(function () {
+          test.Utils.updateMapData(objIdCourse, { COURSE_REPETITION_REFERENCE: courseRef }).then(function success(updateMapDataResult) {
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("should throw if executed without 'objId'", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_learning_function_CreateRepetitionEnrollment", {
+          }).then(function success(jsonResult) {
+            fail(jsonResult);
+            done();
+          }, function error(err) {
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("create repetition enrollment", function (done) {
+        expect(function () {
+          test.Utils.execute("RF_sol_learning_function_CreateRepetitionEnrollment", {
+            objId: objIdEnr
+          }).then(function success(jsonResult) {
+            expect(jsonResult.result.data.objId).toBeDefined();
+            objIdEnrRep = jsonResult.result.data.objId;
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("setTimeout (wait for elo as)", function (done) {
+        expect(function () {
+          test.Utils.setTimeout(interval).then(function success(timeoutResult) {
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("setTimeout (wait for elo as)", function (done) {
+        expect(function () {
+          test.Utils.setTimeout(interval).then(function success(timeoutResult) {
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("setTimeout (wait for elo as)", function (done) {
+        expect(function () {
+          test.Utils.setTimeout(interval).then(function success(timeoutResult) {
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("remove workflow", function (done) {
+        expect(function () {
+          test.Utils.getFinishedWorkflows().then(function success(wfs) {
+            test.Utils.removeFinishedWorkflows(wfs).then(function success1(removeFinishedWorkflowsResult) {
+              done();
+            }, function error(err) {
+              fail(err);
+              console.error(err);
+              done();
+            }
+            );
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+      it("remove repetition enrollment", function (done) {
+        expect(function () {
+          test.Utils.deleteSord(objIdEnrRep).then(function success2(deleteResult) {
+            done();
+          }, function error(err) {
+            fail(err);
+            console.error(err);
+            done();
+          }
+          );
+        }).not.toThrow();
+      });
+    });
+  });
+  afterAll(function (done) {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    expect(function () {
+      test.Utils.getTempfolder().then(function success(tempfolder) {
+        test.Utils.deleteSord(tempfolder).then(function success1(deleteResult) {
+          done();
+        }, function error(err) {
+          fail(err);
+          console.error(err);
+          done();
+        }
+        );
+      }, function error(err) {
+        fail(err);
+        console.error(err);
+        done();
+      }
+      );
+    }).not.toThrow();
+  });
+});
